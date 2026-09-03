cc65d58262bb150074949ad805b39f47f841b83a
lalitsuthar45 <sutharlalit791@gmail.com>
2026-09-03 11:59:52 +0530

    upadete dashboard and cart page file

--- project/src/components/Contact.tsx
+++ project/src/components/Contact.tsx
@@ -100,7 +100,7 @@ export default function Contact() {
 
           <div className="bg-white rounded-2xl shadow-xl overflow-hidden h-[420px]">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.1422937950147!2d-73.98731968459391!3d40.758895979328826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855c6480299%3A0x55194ec5a1ae072e!2sTimes%20Square!5e0!3m2!1sen!2sus!4v1635959887814!5m2!1sen!2sus"
+              src="https://www.google.com/maps?q=Jalore,+Rajasthan,+India&output=embed"
               width="100%"
               height="100%"
               style={{ border: 0 }}
